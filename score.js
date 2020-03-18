612b1a552ef6520dcf827602c799e045d994fd7f
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-03-18 17:33:48 +0800

    up

--- score.js
+++ score.js
@@ -6,17 +6,31 @@ let p2ScoreDisplay = document.getElementById("p2ScoreDisplay");
 
 let p1Score = 0;
 let p2Score = 0;
+let gameOver = false; 
+let winningScore = 5;
 
 p1Btn.addEventListener("click", function(){
-    p1Score = p1Score + 1;
-    console.log(p1Score);
-p1ScoreDisplay.textContent = p1Score;
+    if(!gameOver){
+        p1Score = p1Score + 1;
+        console.log(p1Score);
+        if(p1Score === winningScore) {
+          gameOver = true;
+        }
+        
+    p1ScoreDisplay.textContent = p1Score;
+    }
+  
 });
 
 p2Btn.addEventListener("click", function(){
-    p2Score = p2Score + 1;
-    console.log(p1Score);
-    p2ScoreDisplay.textContent = p2Score;
+    if(!gameOver){
+        p2Score = p2Score + 1;
+        console.log(p1Score);
+        if(p2Score === winningScore) {
+            gameOver = true;
+        }
+        p2ScoreDisplay.textContent = p2Score;
+    }
 });
 
 
